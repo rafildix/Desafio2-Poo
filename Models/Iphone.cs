d7a6f9539770e2d825a6da9db5c1a56a30d40d4b
rafildix <rssousa@outlook.com>
2023-12-25 12:54:41 -0300

    Primeiro Commit do Desafio 2

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string IMEI, int memoria) : base (numero, modelo, IMEI, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo '{nomeApp}' no Iphone...");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria um novo Nokia
+            Nokia nokia = new Nokia("1234567890", "Nokia X20", "1234567890123456", 128);
+            Iphone i1 = new Iphone("156455949", "Iphone 10", "15464941321326", 64);
+
+            // Instala um aplicativo
+            Console.WriteLine("Smartphone Nokia: ");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+            i1.ReceberLigacao();
+            i1.InstalarAplicativo("Telegram");
+
+
+        }
+    }
+}
